611d112febb371d795d2b74d4b86d7489870b877
Panteleev Roman <128892275+JustPanter@users.noreply.github.com>
2023-06-12 14:48:41 +0700

    Оптимизирован код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам
+
+void FinalTest()
+{
+Console.WriteLine("Введите исходный массив через пробел: ");
+string text = Console.ReadLine();
+string[] existArray = text.Split(" ");
+string[] newArray = existArray.Where(text=>text.Length<4).ToArray();
+Console.WriteLine("Итоговый массив значений, длина которых меньше либо равна 3:  ");
+Console.WriteLine(String.Join(" ", newArray));
+}
+FinalTest();
